ed38aba126620fd9b38d8703e1887dd3c54d3f96
Suvam Dolai <drolex2004@gmail.com>
2025-12-03 19:31:40 +0530

    Implement a new function
    Added functionality to retrieve and compare the current MAC address with new mac address to see is it actually changed or not

--- Mac_changer.py
+++ Mac_changer.py
@@ -2,9 +2,10 @@
 import argparse
 import subprocess
 import shlex
+import re
 
 def get_arguments():
-    parser=argparse.ArgumentParser(description="mac changer")
+    parser=argparse.ArgumentParser(description="mac changer") 
     parser.add_argument("-i","--interface",dest="iface",help="network_interface",metavar="",required=True)
     parser.add_argument("-m ","--mac",dest="New_Mac",help="New MAC address",metavar="",required=True)
     parser.add_argument("-os","--os", choices=["linux"], help="eg: -os linux", metavar="",required=True)
@@ -20,8 +21,24 @@ def chane_mac(iface,New_Mac):
     subprocess.call(down)
     subprocess.call(change)
     subprocess.call(up)
-    print(change)
+    
+
+def current_mac(iface):
+    result=subprocess.check_output(shlex.split(f"ifconfig {iface}"),text=True) 
+    mac_result=re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",result)
+    return mac_result.group(0)
+
 
 
 options=get_arguments()
 chane_mac(options.iface,options.New_Mac)
+
+curr_mac=current_mac(options.iface)
+print(f"current mac is {curr_mac}")
+
+
+if curr_mac==options.New_Mac:
+    print(f"[+] MAC address successfully changed to {options.New_Mac}")
+else:
+    print(f"[-] MAC address did not changed")
+
